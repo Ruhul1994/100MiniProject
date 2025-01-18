f4f5d604cdbc37d56c834172043ff8e3607f0b69
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-01-18 13:08:39 +0600

    some Boolean method practice

---
+++ script.js
@@ -0,0 +1,26 @@
+/* 1. Function to return the sum of two numbers
+function sum(a, b) {
+return a + b;
+}
+
+// 2. Function to reverse a string
+function reverseString(str) {
+    // Your code here
+}
+
+// 3. Function to return the largest number in an array
+function findLargestNumber(arr) {
+    // Your code here
+}
+
+
+const formate =true;
+if (formate) {
+    console.log('Hello World');
+}
+*/
+ console.log(Boolean(null));
+ 
+ let distance = 29.0009;
+ console.log(distance.toFixed(2));
+ 
